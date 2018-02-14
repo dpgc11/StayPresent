175e7385afd8f95c3604070d6b5f9a34b3b52eb8
yogeshTelang <yogesh@lifinglabs.com>
2018-02-14 18:02:27 +0530

    add some skeleton code for push notifications

--- app/src/main/java/com/sktrjay/staypresent/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/sktrjay/staypresent/service/MyFirebaseMessagingService.java
@@ -1,40 +1,163 @@
 package com.sktrjay.staypresent.service;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.support.v4.content.LocalBroadcastManager;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.sktrjay.staypresent.utils.AppConstants;
+import com.sktrjay.staypresent.utils.NotificationUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import timber.log.Timber;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    private NotificationUtils notificationUtils;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Timber.d("From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Timber.d("Message data payload: " + remoteMessage.getData());
-
-//            if (/* Check if data needs to be processed by long running job */ true) {
-//                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-//                scheduleJob();
-//            } else {
-//                // Handle message within 10 seconds
-//                handleNow();
-//            }
-
-        }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
-            Timber.d("Message Notification Body: " + remoteMessage.getNotification().getBody());
+            Timber.d("Notification Body: " + remoteMessage.getNotification().getBody());
+            String message = remoteMessage.getNotification().getBody();
+
+            handleNotification(remoteMessage.getNotification().getBody());
         }
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Timber.d("Data Payload: " + remoteMessage.getData().toString());
+
+            try {
+                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+                handleDataMessage(json);
+            } catch (Exception e) {
+                Timber.e( "Exception: " + e.getMessage());
+            }
+        }
     }
+
+    private void handleNotification(String message) {
+        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+//            // app is in foreground, broadcast the push message
+//            Intent pushNotification = new Intent(AppConstants.PUSH_NOTIFICATION);
+//            pushNotification.putExtra("message", message);
+//            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+////
+////            // play notification sound
+//            NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+//            notificationUtils.playNotificationSound();
+//            sendNotification("lemonop", message);
+        } else {
+            // If the app is in background, firebase itself handles the notification
+            vibrateDevice();
+        }
+    }
+
+    private void handleDataMessage(JSONObject json) {
+        Timber.d("push json: " + json.toString());
+
+        try {
+            JSONObject data = json.getJSONObject("data");
+
+
+            Timber.d("handleDataMessage: dataObject: " + data);
+//            String title = json.getString("titleKey");
+            String title = data.getString("title");
+            String message = data.getString("message");
+//            String message = "MESSAGE";
+            boolean isBackground = data.getBoolean("is_background");
+            String imageUrl = data.getString("image");
+            String timestamp = data.getString("timestamp");
+            JSONObject payload = data.getJSONObject("payload");
+
+            Timber.d("title: " + title);
+            Timber.d("message: " + message);
+            Timber.d("isBackground: " + isBackground);
+            Timber.d("payload: " + payload.toString());
+            Timber.d("imageUrl: " + imageUrl);
+            Timber.d("timestamp: " + timestamp);
+
+
+            if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+                // app is in foreground, broadcast the push message
+                Intent pushNotification = new Intent(AppConstants.PUSH_NOTIFICATION);
+                pushNotification.putExtra("message", message);
+                LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+                // play notification sound
+                NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+                notificationUtils.playNotificationSound();
+//                sendNotification(title, message);
+            } else {
+                // app is in background, show the notification in notification tray
+//                Intent resultIntent = new Intent(getApplicationContext(), EditFullProfileActivity.class);
+//                resultIntent.putExtra("message", message);
+//
+//                // check for image attachment
+//                if (TextUtils.isEmpty(imageUrl)) {
+//                    showNotificationMessage(getApplicationContext(), title, message, timestamp, resultIntent);
+//                } else {
+//                    // image is present, show notification with image
+//                    showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, resultIntent, imageUrl);
+//                }
+            }
+        } catch (JSONException e) {
+            Timber.e("Json Exception: " + e.getMessage());
+        } catch (Exception e) {
+            Timber.e("Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Showing notification with text only
+     */
+    private void showNotificationMessage(Context context, String title, String message, String timeStamp, Intent intent) {
+        notificationUtils = new NotificationUtils(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        notificationUtils.showNotificationMessage(title, message, timeStamp, intent);
+    }
+
+    /**
+     * Showing notification with text and image
+     */
+    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
+        notificationUtils = new NotificationUtils(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
+    }
+
+    private void sendNotification(String title, String messageBody) {
+//        Intent intent = SplashScreenActivity.getStartIntent(this);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+//                PendingIntent.FLAG_ONE_SHOT);
+//
+//        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(R.drawable.ic_stat_logoapp)
+//                .setContentTitle(title)
+//                .setContentText(messageBody)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent);
+//
+//        NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    private void vibrateDevice() {
+        Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+    }
+
 }
---
+++ app/src/main/java/com/sktrjay/staypresent/utils/AppConstants.java
@@ -0,0 +1,14 @@
+package com.sktrjay.staypresent.utils;
+
+/**
+ * Created by yogesh on 14/2/18.
+ */
+
+public final class AppConstants {
+
+    // id to handle the notification in the notification tray
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+}
